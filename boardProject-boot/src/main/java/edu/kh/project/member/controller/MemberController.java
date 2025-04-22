02a40a089cd5281015e71da892c5e2ee83d323e7
ashtiger1999 <aa26164095@gmail.com>
2025-04-22 10:20:45 +0900

    login:saveId-checkBox

--- boardProject-boot/src/main/java/edu/kh/project/member/controller/MemberController.java
+++ boardProject-boot/src/main/java/edu/kh/project/member/controller/MemberController.java
@@ -3,13 +3,18 @@ package edu.kh.project.member.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.project.member.model.dto.Member;
 import edu.kh.project.member.model.service.MemberService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @SessionAttributes({"loginMember"})
@@ -47,7 +52,13 @@ public class MemberController {
 	@PostMapping("login")
 	public String login(Member inputMember,
 						RedirectAttributes ra,
-						Model model) {
+						Model model,
+						@RequestParam(value="saveId", required = false) String saveId,
+						HttpServletResponse resp) {
+		
+		// 체크박스
+		// - 체크가   된 경우 : "on"
+		// - 체크가 안된 경우 : null
 		
 		// 로그인 서비스 호출
 		Member loginMember = service.login(inputMember);
@@ -65,8 +76,48 @@ public class MemberController {
 			// 2단계 : 클래스 위에 @SessionAttributes() 어노테이션을 작성하여
 			//		   session scope로 이동
 			
+			// *********** Cookie ***********
+			// 로그인 시 작성한 이메일 저장 (쿠키에)
+			
+			// 쿠키 객체 생성(K:V)
+			Cookie cookie = new Cookie("saveId", loginMember.getMemberEmail());
+			// saveId=user01@kh.or.kr
+			
+			// 쿠키가 적용될 경로 설정
+			// -> 클라이언트가 어떤 요청을 할 떄 쿠기를 첨부할 지 지정
+			// ex) "/" : IP 또는 도메인 또는 localhost
+			// 			 -> 메인페이지 + 그 하위의 모든 주소
+			cookie.setPath("/");
+			
+			// 쿠키의 만료 기간 지정
+			if(saveId != null) { // 아이디 저장 체크박스 체크했을 떄
+				
+				cookie.setMaxAge(60*60*24*30); // 30일 (초 단위 지정)
+				
+			} else { // 체크하지 않았을떄
+				
+				cookie.setMaxAge(0);
+				
+			}
+			
+			// 응답 객체에 쿠키 추가 -> 클라이언트에게 전달
+			resp.addCookie(cookie);
+			
 		}
 		
 		return "redirect:/"; // 메인페이지 재요청
+	}
+	
+	/** 로그아웃 : session에 저장된 로그인된 회원 정보를 없앰
+	 * @param SessionStatus status : 
+	 * 			SessionAttributes로 지정된 특정 속성을 세선에서 제거하는 기능    
+	 * @return
+	 */
+	@GetMapping("logout")
+	public String logout(SessionStatus status) {
+	
+		status.setComplete(); // 세션을 완료 시킨(==@SessionAttributes로 등록된 세션 제거
+		
+		return "redirect:/";
 	}
 }
