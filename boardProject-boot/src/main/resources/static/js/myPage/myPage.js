8e3cd760154979034328be95381595d68560f2e7
ashtiger1999 <aa26164095@gmail.com>
2025-04-24 19:31:34 +0900

    t

--- boardProject-boot/src/main/resources/static/js/myPage/myPage.js
+++ boardProject-boot/src/main/resources/static/js/myPage/myPage.js
@@ -1,3 +1,36 @@
+console.log("test");
+
+// 다음 주소 API 다루기
+function execDaumPostcode() {
+    new daum.Postcode({
+        oncomplete: function (data) {
+            // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+            // 각 주소의 노출 규칙에 따라 주소를 조합한다.
+            // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+            var addr = ''; // 주소 변수
+
+            //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+            if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                addr = data.roadAddress;
+            } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                addr = data.jibunAddress;
+            }
+
+            // 우편번호와 주소 정보를 해당 필드에 넣는다.
+            document.getElementById('postcode').value = data.zonecode;
+            document.getElementById("address").value = addr;
+            // 커서를 상세주소 필드로 이동한다.
+            document.getElementById("detailAddress").focus();
+        }
+    }).open();
+}
+
+const searchAddress = document.querySelector("#searchAddress");
+if(searchAddress!=null) {
+    searchAddress.addEventListener("click", execDaumPostcode);
+}
+
 /* 회원 정보 수정 페이지 */
 const updateInfo = document.querySelector("#updateInfo"); // form 태그
 
@@ -104,6 +137,7 @@ if (updateInfo != null) {
 // 비밀번호 변경 form 태그
 const changePw = document.querySelector("#changePw");
 
+// 현재 페이지에서 changePw 요소가 존재할 때
 if (changePw != null) {
     // 제출 되었을 때
     changePw.addEventListener("submit", e => {
