7965963523d40aebe8f670e2a4960b97830d5120
ashtiger1999 <aa26164095@gmail.com>
2025-05-07 17:56:34 +0900

    post update

---
+++ boardProject-boot/src/main/java/edu/kh/project/board/model/service/EditBoardService.java
@@ -0,0 +1,27 @@
+package edu.kh.project.board.model.service;
+
+import java.rmi.server.ExportException;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import edu.kh.project.board.model.dto.Board;
+
+public interface EditBoardService {
+
+	/** 게시글 작성 서비스
+	 * @param inputBoard
+	 * @param images
+	 * @return
+	 */
+	int boardInsert(Board inputBoard, List<MultipartFile> images) throws Exception;
+
+	/** 게시글 수정 서비스
+	 * @param inputBoard
+	 * @param images
+	 * @param deleteOrderList
+	 * @return
+	 */
+	int boardUpdate(Board inputBoard, List<MultipartFile> images, String deleteOrderList) throws Exception;
+
+}
